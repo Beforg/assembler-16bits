1982804bd9483c8c74b740c5150c59f019845fcc
Beforg <bruno.capsqn@gmail.com>
2025-07-04 22:32:30 -0300

    refactor: circuito -Adicionando Banco de Registradores

--- assembler.c
+++ assembler.c
@@ -1,7 +1,7 @@
 /*
  * assembler.c
  * 
- * Copyright 2025 Bruno Forgiarini <www.beforg.my>
+ * Copyright 2025, Desenvolvido por Bruno Forgiarini <www.beforg.my>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
