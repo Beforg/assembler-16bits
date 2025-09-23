4678044c88d5ea4e0c3e4b31729f8459de0852f1
netoe1 <elynetobr@outlook.com>
2025-09-23 12:05:13 -0300

    prototip ProgramMemory.ts

--- PDI-Simulator/classes/ProgramMemory.ts
+++ PDI-Simulator/classes/ProgramMemory.ts
@@ -1,1 +1,11 @@
-class ProgramMemory{};
+import { ProgramCounter } from "./ProgramCounter";
+
+class ProgramMemory {
+  public _pc: ProgramCounter;
+  public _addresses: number[];
+  private _mem_size: number;
+  constructor(__mem_size:number){
+    // Instanciar PC
+    this._pc = new ProgramCounter(0,16);
+  }
+}
