22878b3536b97dacd33ecb27577ffe47cf47119c
Beforg <bruno.capsqn@gmail.com>
2025-10-18 09:12:33 -0300

    alterações interface ide

--- compilador/compilador-arquitetura-16bits/src/main/java/com/unipampa/compiladorarquitetura16bits/CompilerApplication.java
+++ compilador/compilador-arquitetura-16bits/src/main/java/com/unipampa/compiladorarquitetura16bits/CompilerApplication.java
@@ -11,7 +11,7 @@ public class CompilerApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CompilerApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1200, 1200);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Compilador Arquitetura 16 bits");
         stage.setScene(scene);
         stage.show();
